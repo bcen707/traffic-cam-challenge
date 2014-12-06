214a3b49756e5d35e29b9820ab4a2759f601e952
bcen707 <bcen707@lt-ougl-3-12.eplt.washington.edu>
2014-12-05 20:25:17 -0800

    edited js file

--- js/app.js
+++ js/app.js
@@ -11,3 +11,17 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+function onReady() {
+
+	var infoWin = new google.maps.InfoWindow();
+
+	var mapOptions = {
+		center: {lat: 47.6, lng: -122.3}
+		zoom: 12;
+	}
+
+	var mapElem = document.getElementById('map');
+
+	var map = new google.maps.Map(mapElem, mapOptions);
+
+} //onReady
